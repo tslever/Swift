1a16a54d1dfc4be5af68d11e748da63ebe392d12
Tom Lever <46533664+tslever@users.noreply.github.com>
2024-07-28 11:52:05 -0400

    Returned function

--- A_Swift_Tour.swift
+++ A_Swift_Tour.swift
@@ -221,3 +221,12 @@ func returnFifteen() -> Int {
     return y
 }
 print(returnFifteen())
+
+func makeIncrementer() -> ((Int) -> Int) {
+    func addOne(number: Int) -> Int {
+        return 1 + number
+    }
+    return addOne
+}
+var increment = makeIncrementer()
+print(increment(7))
