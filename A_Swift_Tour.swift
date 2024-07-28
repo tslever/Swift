7848c0c2f1ebe37d6680a413a299c2b477105cd9
Tom Lever <46533664+tslever@users.noreply.github.com>
2024-07-28 16:16:24 -0400

    Used willSet

--- A_Swift_Tour.swift
+++ A_Swift_Tour.swift
@@ -395,3 +395,27 @@ var triangle: EquilateralTriangle = EquilateralTriangle(sideLength: 3.1, name: "
 print(triangle.perimeter)
 triangle.perimeter = 9.9
 print(triangle.sideLength)
+
+/*the class below ensures that the side length of its triangle
+is always the same as the side length of its square.*/
+class TriangleAndSquare {
+    var triangle: EquilateralTriangle {
+        willSet {
+            square.sideLength = newValue.sideLength
+        }
+    }
+    var square: Square {
+        willSet {
+            triangle.sideLength = newValue.sideLength
+        }
+    }
+    init(size: Double, name: String) {
+        square = Square(sideLength: size, name: name)
+        triangle = EquilateralTriangle(sideLength: size, name: name)
+    }
+}
+var triangleAndSquare: TriangleAndSquare = TriangleAndSquare(size: 10, name: "another test shape")
+print(triangleAndSquare.square.sideLength)
+print(triangleAndSquare.triangle.sideLength)
+triangleAndSquare.square = Square(sideLength: 50, name: "larger square")
+print(triangleAndSquare.triangle.sideLength)
