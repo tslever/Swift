85bf04467399f87c97adf5dc2eadd59a94f5c958
Tom Lever <46533664+tslever@users.noreply.github.com>
2024-07-28 15:52:30 -0400

    Used initializer

--- A_Swift_Tour.swift
+++ A_Swift_Tour.swift
@@ -315,3 +315,14 @@ var improvedShape: ImprovedShape = ImprovedShape()
 improvedShape.setNumberOfSides(numberOfSidesToUse: 5)
 var improvedShapeDescription: String = improvedShape.simpleDescription()
 print(improvedShapeDescription)
+
+class NamedShape {
+    var numberOfSides: Int = 0
+    var name: String
+    init(name: String) {
+        self.name = name
+    }
+    func simpleDescription() -> String {
+        return "A shape with \(numberOfSides) sides"
+    }
+}
