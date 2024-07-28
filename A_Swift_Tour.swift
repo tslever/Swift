1e13d559f28cb95182bcabfec005bdff5a85a475
Tom Lever <46533664+tslever@users.noreply.github.com>
2024-07-28 16:10:43 -0400

    Got and set

--- A_Swift_Tour.swift
+++ A_Swift_Tour.swift
@@ -369,3 +369,29 @@ let circle: Circle = Circle(radius: 5.2, name: "my test circle")
 print(circle.area())
 print(circle.simpleDescription())
 
+class EquilateralTriangle: NamedShape {
+    var sideLength: Double = 0.0
+
+    init(sideLength: Double, name: String) {
+        self.sideLength = sideLength
+        super.init(name: name)
+        numberOfSides = 3
+    }
+
+    var perimeter: Double {
+        get {
+            return 3.0 * sideLength
+        }
+        set {
+            sideLength = newValue / 3.0
+        }
+    }
+
+    override func simpleDescription() -> String {
+        return "An equilateral triangle with sides of length \(sideLength)"
+    }
+}
+var triangle: EquilateralTriangle = EquilateralTriangle(sideLength: 3.1, name: "a triangle")
+print(triangle.perimeter)
+triangle.perimeter = 9.9
+print(triangle.sideLength)
